0fb9fc2e1623222d1f3b44ff8eb3c2e040fa81fa
mtk14n <mtk14minou@gmail.com>
2026-08-27 01:25:43 +0200

    fix: ci.yml to horchestrate the all ci of the project on push and merge request

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-  return {"status": "ok", "app": "taskflow-api"}
+    return {"status": "ok", "app": "taskflow-api"}
+
 
 @app.get("/health")
 def health_check():
-  return {"status": "healthy"}
+    return {"status": "healthy"}
